f677037b34c4a8c0d0a06bb3a8ff703526711a35
WillDyer <37468008+WillDyer@users.noreply.github.com>
2024-05-28 13:32:27 +0100

    Update ribbon_it.py
    added controls and control joints

--- Ribbon/ribbon_it.py
+++ Ribbon/ribbon_it.py
@@ -1,67 +1,37 @@
 import maya.cmds as cmds
 import maya.OpenMaya as om
+import importlib
+import sys, os, math
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-def param_from_length(curve, count, curve_type = "open", space = "uv", normalized=True):
-
-    if curve_type == "periodic":
-        divider = count
-    else:
-        divider = count - 1
-
-    if divider==0:
-        divider = 1
-       
-    dag = om.MDagPath()
-    obj = om.MObject()
-    crv = om.MSelectionList()
-    crv.add(curve)
-    crv.getDagPath(0, dag, obj)
-
-    curve_fn = om.MFnNurbsCurve(dag)
-    length = curve_fn.length()
-
-    param = [curve_fn.findParamFromLength(i * ((1 / float(divider)) * length)) for i in range(count)]
-
-    if space == "world":
-        data=[]
-        space = om.MSpace.kWorld
-        point = om.MPoint()
-        for p in param:
-            curve_fn.getPointAtParam(p, point, space)
-            data.append([point[0], point[1], point[2]]) #world space points
-    elif normalized == True:
-
-        def normalizer(value, old_range, new_range): 
-            return (value - old_range[0]) * (new_range[1] - new_range[0]) / (old_range[1] - old_range[0]) + new_range[0]
-        
-        max_v = cmds.getAttr(curve + ".minMaxValue.maxValue")
-        min_v = cmds.getAttr(curve + ".minMaxValue.minValue")
-
-        data = [normalizer(p, [min_v, max_v], [0, 1]) for p in param] 
-    else:
-        data = param
-
-    cmds.delete(curve)
-    return data
+import ramp_values
+import OPM
+importlib.reload(ramp_values)
+importlib.reload(OPM)
 
 class ribbon():
     def __init__(self):
         #self.group_setup()
-        self.base_ribbon()
-        
-    def base_ribbon(self):
         self.ctrl_amount = 3
         self.fk_enabled = 1
+        self.selection = cmds.ls(sl=1)
+
+        self.top_grp = cmds.group(n="grp_ribbon_#", em=1)
+        self.base_ribbon()
+        self.create_controllers()
+        self.group_setup()
+        self.skin_ribbon()
+        cmds.select(clear=1)
+
+    def base_ribbon(self):
         
         # Collect span count
-        selection = cmds.ls(sl=1)
-        print(f"selection: {selection[0]}")
-        surface = cmds.listRelatives(selection[0],s=1)[0]
+        print(f"selection: {self.selection[0]}")
+        surface = cmds.listRelatives(self.selection[0],s=1)[0]
 
-        self.spansU = cmds.getAttr(f"{selection[0]}.spansU")
-        self.spansV = cmds.getAttr(f"{selection[0]}.spansV")
+        self.spansU = cmds.getAttr(f"{self.selection[0]}.spansU")
+        self.spansV = cmds.getAttr(f"{self.selection[0]}.spansV")
 
-        u_curve_corr = cmds.duplicateCurve(selection[0] + ".v[.5]", local=True, ch=0)[0]
         # selected surface is periodic or open? (cylinder or a plane)
         if cmds.getAttr(surface + ".formU") == 2 or cmds.getAttr(surface + ".formV") == 2: # Check for closed
             curve_type = "periodic"
@@ -70,7 +40,9 @@ class ribbon():
             curve_type = "open"
             spans = self.spansU + 1
 
-        param_ctrls = param_from_length(u_curve_corr, spans, curve_type, "uv")
+        u_curve_corr = cmds.duplicateCurve(self.selection[0] + ".v[.5]", local=True, ch=0)[0]
+        param_ctrls = ramp_values.param_from_length(u_curve_corr, spans, curve_type, "uv", delete_curve=False)
+        self.skn_ctrls = ramp_values.param_from_length(u_curve_corr, self.ctrl_amount, curve_type, "uv", delete_curve=True)
 
         self.fol_shape_list = []
         self.fol_parent_list = []
@@ -78,9 +50,6 @@ class ribbon():
             fol_shape = cmds.createNode("follicle")
             fol_parent = cmds.listRelatives(fol_shape, p=True)[0]
             cmds.setAttr(fol_shape + ".visibility", 1)
-
-            #print(fol_shape)
-            #print(fol_parent)
 
             cmds.setAttr(f"{fol_shape}.simulationMethod",0)
             
@@ -95,7 +64,45 @@ class ribbon():
 
             cmds.select(fol_parent)
             cmds.joint(n=f"jnt_fol_{fol_parent}")
-            #cmds.select(cl=1)
 
             self.fol_shape_list.append(fol_shape)
             self.fol_parent_list.append(fol_parent)
+
+    def create_controllers(self):
+        cmds.select(cl=1)
+
+        self.ctrl_list = []
+        self.skn_jnt_list = []
+
+        fol_list_int = len(self.fol_parent_list)
+        for x in self.skn_ctrls:
+            item = fol_list_int * x
+            item = math.ceil(item)
+            if item == 0:
+                item = 1
+
+            selected_follicle = self.fol_parent_list[item-1]
+            cmds.select(selected_follicle)
+            cmds.joint(n=f"jnt_ctrl_{selected_follicle}",r=3)
+            cmds.parent(f"jnt_ctrl_{selected_follicle}",world=True)
+            cmds.select(cl=1)
+            self.skn_jnt_list.append(f"jnt_ctrl_{selected_follicle}")
+
+            ctrl = f"ctrl_{selected_follicle}"
+            cmds.circle(n=ctrl,r=1) # temporary, will make interchangable
+            cmds.matchTransform(ctrl,f"jnt_ctrl_{selected_follicle}")
+            cmds.parentConstraint(ctrl,f"jnt_ctrl_{selected_follicle}",mo=1, n=f"pConst_jnt_ctrl_{selected_follicle}")
+            cmds.connectAttr(f"{ctrl}.scale", f"jnt_ctrl_{selected_follicle}.scale")
+            cmds.select(ctrl)
+            OPM.offsetParentMatrix()
+            self.ctrl_list.append(ctrl)
+
+    def group_setup(self):
+        cmds.group(self.ctrl_list, n="grp_ribbon_ctrl", p=self.top_grp)
+        cmds.group(self.skn_jnt_list, n="grp_ribbon_skn_jnt", p=self.top_grp)
+        cmds.group(self.fol_parent_list, n="grp_ribbon_fol", p=self.top_grp)
+
+    def skin_ribbon(self):
+        print(self.skn_jnt_list)
+        print(self.selection[0])
+        cmds.skinCluster(self.skn_jnt_list, self.selection[0])
