8ed40c0efc1eb3473d95d35f67591cb25d37e8b2
WillDyer <will.dyer9@gmail.com>
2024-07-09 15:19:20 +0100

    init

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from Ribbon_Twist_Joints import code # change to script running temp for now
+from Ribbon import ribbon_it
 
 importlib.reload(code) # remove after debug
 
 def run():
-    code.main()
+    ribbon_it.main()
     print("main run")
     
 # run_ui()
